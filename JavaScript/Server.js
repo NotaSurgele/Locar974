ce34128f31f8bc7a0b3631c269f28801dc50cc48
alexis <alexis.picard@epitech.eu>
2020-08-24 17:25:38 +0400

    save

--- JavaScript/Server.js
+++ JavaScript/Server.js
@@ -1,25 +1,10 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+var querystring = require('querystring');
 
 const HOST = 'localhost'
 const PORT = 8080;
-
-const option = {
-    method: 'GET'
-}
-
-function GET_() {
-    const req = http.request(option, res => {
-        console.log(`statusCode: ${res.statusCode}`)
-
-        res.on('data', data => {
-            process.stdout.write(data)
-        });
-    });
-    req.on('error', error => {
-        console.error(error)
-      })
-}
 
 
 fs.readFile('../HTML/index.html', function (err, html) {
@@ -27,10 +12,21 @@ fs.readFile('../HTML/index.html', function (err, html) {
     if (err) throw err;
 
     console.log("Server is listening on " + HOST + ":" + PORT);
+
     http.createServer(function(req, res) {
-        GET_();
+        var url_parts = url.parse(req.url, true);
+        var query = url_parts.query;
+        console.log(query);
+        var parsedQuery = querystring.stringify(query);
+        fs.appendFile('data.txt', "TEST  ", (err) => {
+            if (err) throw err;
+        });
         res.writeHeader(200, {"Content-Type": "text/html"});
         res.write(html);
         res.end();
+
+
     }).listen(PORT, HOST);
+
+
 });
