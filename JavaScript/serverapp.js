a55d7793991cd35946a3b7983cd8ed731a8dbdd0
alexis <alexis.picard@epitech.eu>
2020-08-26 15:39:12 +0400

    Implémented  sqlite3 data base

--- JavaScript/serverapp.js
+++ JavaScript/serverapp.js
@@ -6,6 +6,22 @@ const bodyParser = require('body-parser');
 var path = require("path");
 const {check, validationResult} = require('express-validator');
 
+var sqlite = require('sqlite3').verbose();
+
+// open and create sqlite data base connexion
+let db = new sqlite.Database(':memory', (err) => {
+    if (err) return console.error("cannot connect to database error : " + err);
+
+    console.log("connected to the data base");
+});
+
+//close sqlite database connexion
+db.close((err) => {
+    if (err) console.error("cannot close the database error: " + err);
+
+    console.log("Data base close sucessfuly");
+});
+
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("../HTML/Pic"));
 
@@ -28,7 +44,7 @@ app.post('/submit_form', [
     const email = req.body.email;
     const password = req.body.password;
     console.log(email + ' ' + password);
-    res.send("formulaire envoté");
+    res.send("formulaire envoté avec l'adresse : " + email + "et comme mdp :" + password);
 });
 
 
