62cb0642c0ad54d8eeb9c0d69be0900566ff5019
alexis <alexis.picard@epitech.eu>
2020-08-27 09:48:08 +0400

    save

--- JavaScript/serverapp.js
+++ JavaScript/serverapp.js
@@ -5,16 +5,15 @@ const PORT = 8080;
 const bodyParser = require('body-parser');
 var path = require("path");
 const {check, validationResult} = require('express-validator');
-
+const data = "../database/data.db";
 var sqlite = require('sqlite3').verbose();
 
-// open and create sqlite data base connexion
-let db = new sqlite.Database('../database/data.db', (err) => {
-    if (err) return console.error("cannot connect to database error : " + err);
 
+// open and create sqlite data base connexion
+let db = new sqlite.Database(data, (err) => {
+    if (err) return console.error("cannot connect to database error : " + err);
     console.log("connected to the data base");
 });
-
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("../HTML/Pic"));
@@ -38,26 +37,22 @@ app.post('/submit_form', [
         const password = req.body.password;
         console.log(email + ' ' + password);
         db.run(`INSERT INTO users (email, password) values ('${email}', '${password}')`, (err) =>{
-            if (err) return console.error("Cannot insert into the TABLE" + err.message);
+            if (err) return console.error("Cannot insert values into the TABLE" + err.message);
         });
         console.log(email + password + " has been added to the data base");
-        db.close();
+        // db.close();
         res.send("formulaire envoté avec l'adresse : " + email + "et comme mdp :" + password);
     }
-
+    let db = new sqlite.Database(data, (err) => {
+        if (err) return console.error("cannot connect to database error : " + err);
+        console.log("connected to the data base");
+    });
 });
 
-
-app.get ('/new', (req, res) => {
+app.get('/new', (req, res) => {
     res.sendFile(path.join(__dirname + '/../HTML/test.html'), function (err) {
         if (err) throw err;
     });
 });
-// //close sqlite database connexion
-// db.close((err) => {
-//     if (err) console.error("cannot close the database error: " + err);
-
-//     console.log("Data base close sucessfuly");
-// });
 
 app.listen(PORT,console.log('App listening on localhost:'+ PORT));
